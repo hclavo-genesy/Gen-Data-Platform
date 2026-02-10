9d1c3f9bfd901fb905866fee7c3ab14ccbc5f233
hclavo@genesysworks.org <hclavo@genesysworks.org>
2026-02-10 20:07:52 +0000

    changes

--- macros/refresh_holidays.sql
+++ macros/refresh_holidays.sql
@@ -1,12 +1,5 @@
 {% macro refresh_holidays(country_code='US') %}
-    {% set sql%}
-        CALL ANALYTICS_DEV.SILVER.SP_REFRESH_HOLIDAYS(' {{country_code}}');
-    {% endset %%}
-
-    {{ log ("Running: " ~ sql, info=True) }}
-
-    {% set res = run_query(sql) %}
-
-    {{ log"Result: " ~ (res | string), info=True  }}
-
+  {% do run_query(
+    "CALL ANALYTICS_DEV.SILVER.SP_REFRESH_HOLIDAYS('" ~ country_code ~ "');"
+  ) %}
 {% endmacro %}
